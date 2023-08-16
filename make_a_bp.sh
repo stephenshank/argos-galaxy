cd2f7d2a55dab215f6a68462e5a9dfc7cfac9829
Stephen Shank <sshank314@gmail.com>
2023-08-16 13:44:48 -0400

    Update make_a_bp script to not fetch when there are no assemblies.

--- make_a_bp.sh
+++ make_a_bp.sh
@@ -2,10 +2,16 @@
 
 bp_acc=$1
 echo Making TSV for $bp_acc...
-alias sm='snakemake --rerun-triggers mtime --rerun-incomplete -j 1 -k'
+alias sm='snakemake --rerun-triggers mtime --rerun-incomplete -j 3 -k'
 assembly_accessions=data/ncbi/bioproject/$bp_acc/links/assembly/accessions.txt
-sm $assembly_accessions
-sm $(sed 's/.*/data\/ncbi\/assembly\/&\/biosample.txt/' $assembly_accessions)
-tsv=data/ncbi/bioproject/$bp_acc/assemblyqc_input.tsv
-sm $tsv
+assembly_content=$(cat $assembly_accessions)
+if [ "$assembly_content" != "null" ]; then
+  echo "Fetching assembly data for $bp_acc..."
+  sm $assembly_accessions
+  sm $(sed 's/.*/data\/ncbi\/assembly\/&\/biosample.txt/' $assembly_accessions)
+  tsv=data/ncbi/bioproject/$bp_acc/assemblyqc_input.tsv
+  sm $tsv
+else
+  echo "No assemblies for $bp_acc, skipping..."
+fi
 echo ...made $tsv!
